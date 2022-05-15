5cd0a6d9397c951a57ba1dd5e7dc57917bd1257d
Bhavneet Singh <84433782+Bhavneet345@users.noreply.github.com>
2022-05-15 10:28:15 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,24 @@
+// var i = 1;
+// while(i < 5){
+//     console.log(i);
+//     ++i;
+// }
+// console.log("\n");
+// for(i = 1; i < 5; i++){
+//     console.log(i);
+// }
+// console.log("\n");
+// var i , j ;    
+//    for(i=0, j=10; i<5; ++i) { 
+//       console.log("Hello"); 
+//    }
+// console.log("\n");
+//    for(var i=0; i<5; ++i) { 
+//     console.log("Hello") ; 
+//  }
+
+ var i=0 ;     
+   while(i<5); { 
+      console.log("hii"); 
+      ++i ; 
+   }
