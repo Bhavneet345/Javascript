e8e547d823b839f2f2cabd8ad0e677cfb61ae276
Bhavneet Singh <84433782+Bhavneet345@users.noreply.github.com>
2022-05-31 19:55:24 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,17 @@
+var a = 10;
+a += 12;
+console.log(a);
+a += 20;
+console.log(a);
+a++;
+++a;
+console.log(a);
+console.log("a" == "b");
+console.log(1 + '1');
+var c;
+console.log(c + "b");
+console.log(1 - '1')
+console.log(null == undefined)
+console.log(typeof(null) == typeof(undefined));
+console.log(65 + "H");
+console.log(typeof( typeof( typeof( 100 ) ) ));
