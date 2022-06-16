9cb1989c46caff4eac0d1377fab6c1c4f146d3f6
Bhavneet Singh <84433782+Bhavneet345@users.noreply.github.com>
2022-06-16 10:50:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path')
+const port = 8000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get('/', function(req, res){
+    return res.render('home');
+})
+
+app.listen(port, function(err){
+    if(err){
+        console.log(err);
+        return;
+    }
+    console.log("Server running on port", port);
+})
